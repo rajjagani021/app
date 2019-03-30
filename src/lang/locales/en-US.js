29887a7ecfe2b8b048f73a507f814894f7103a4e
Jan Bílek <honza@elision.design>
2019-03-30 23:34:11 +0100

    Add english translation

--- src/lang/locales/en-US.js
+++ src/lang/locales/en-US.js
@@ -451,6 +451,7 @@ export default {
   version: "Version",
   version_and_updates: "Version and Updates",
   view_type: "View As...",
+  visible_all_users: "Visible for all users",
   welcome: "Welcome",
   writable_fields: "Writable Fields",
   writable_fields_copy: "Select the fields that the user can edit",
